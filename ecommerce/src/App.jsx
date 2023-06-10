d8a214c46e8f0ccaa4733d8666ea36916e9be138
yagob2 <127253988+yagob2@users.noreply.github.com>
2023-06-10 19:07:08 -0300

    itemCarrinho

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { DefaultButton } from './common/components/DefaultButton'
+import ItemCarrinho from './common/components/itemCarrinho/itemCarrinho'
 
 function App() {
   return (
     <>
       <div>
         Hello World
+        <ItemCarrinho></ItemCarrinho>
       </div>
       <DefaultButton type={'sucess'}/>
       <DefaultButton type={'warning'}/>
