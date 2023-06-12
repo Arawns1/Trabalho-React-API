50dd9ea706271a1210753bef4f13f9923e476d30
Roberta Stumpf <robertamdstumpf@gmail.com>
2023-06-12 16:54:22 -0300

    Atualização tela de login

--- ecommerce/src/pages/Login/Login.jsx
+++ ecommerce/src/pages/Login/Login.jsx
@@ -2,29 +2,28 @@ import './Login.css'
 
 function Login() {
     return (
-    <div className="container">
-        <div className="container-login">
+        <div className="container">
+            <div className="container-login">
+                <span className="title">Login</span>
                 <form className="login-form">
-                    <span className="title">Login</span>
-
-                    <div className="criarConta">
-                        <span className="texto">Não tem uma conta? Inscreva-se</span>
+                    <div className="input">
+                        <input className="input2" type="email" placeholder="Digite o seu Email" />
                     </div>
                     <div className="input">
-                        <input className="input2" type="email" />
-                    </div>
-                    <div className="input">
-                        <input className="input2" type="senha" />
+                        <input className="input2" type="senha" placeholder="Digite a sua Senha" />
                     </div>
                     <div className="esqueceuSenha">
                         <span className="texto1">Esqueceu sua senha?</span>
                     </div>
                     <div className="container-login-form-btn">
                         <button className="login-form-btn">Entrar</button>
-                         </div>
-                    </form>
-                </div>
+                    </div>
+                    <div className="criarConta">
+                        <span className="texto">Não tem uma conta? Inscreva-se</span>
+                    </div>
+                </form>
             </div>
+        </div>
     )
 }
 
