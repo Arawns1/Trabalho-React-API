293f46d8beb3127cbcab55c0c328c7fdbbd36393
Gabriel Damico <gabrieldamico22@gmail.com>
2023-06-14 17:08:35 -0300

    FeaturedProducts Atualizado

--- ecommerce/src/common/components/featuredProducts/FeaturedProducts.jsx
+++ ecommerce/src/common/components/featuredProducts/FeaturedProducts.jsx
@@ -1,16 +1,14 @@
 /* eslint-disable react/prop-types */
 import { FeaturedCard } from '../../components/cards/featuredCard/FeaturedCard.jsx'
-import { FeatCard,
-  FeaturedCardTitle,
-  FeaturedCardContainer } from './style.js';
+import './style.css'
 
 export function FeaturedProducts(props) {
   return (
     <div className="featured-cards">
       <h3 id="featured-card-title">Produtos em Destaque</h3>
       <div className="featured-cards-container">
-        <FeaturedCard numero={0} product={props.products} />
-        <FeaturedCard numero={2} product={props.products} />
+        <FeaturedCard numero={34} product={props.products} />
+        <FeaturedCard numero={59} product={props.products} />
       </div>
     </div>
   );
